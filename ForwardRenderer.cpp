fa41d4cf59d6bd0a86f65512a046a0d4235cda14
Stephanie <stephaniest1@gmail.com>
2021-01-28 15:32:33 +0100

    Removed extra code and cleaned up structure

--- ForwardRenderer.cpp
+++ ForwardRenderer.cpp
@@ -1,44 +1,68 @@
 #include "ForwardRenderer.h"
-//Sets everything up for forward rendering
-void ForwardRenderer::BeginFrame() {
-	//Clears the color for the scene
-	//Set depth buffer
-	//Set buffers that will be used in the pipeline on thier respective shader
-	//(would propably need an external function that the engine communicates with maybe)
-	//Set shaders to forward rendering
+
+ForwardRenderer::ForwardRenderer() noexcept {
+	this->inputLayout = nullptr;
+	this->vertexShader = nullptr;
+	this->pixelShader = nullptr;
+	this->vertexBuffers = nullptr;
+	this->pixelBuffers = nullptr;
+}
+
+void ForwardRenderer::SetVertexShader(ID3D11InputLayout* inputLayout, ID3D11VertexShader* vertexShader) {
+	this->inputLayout = inputLayout;
+	this->vertexShader = vertexShader;
+}
+
+void ForwardRenderer::SetPixelShader(ID3D11PixelShader* pixelShader) {
+	this->pixelShader = pixelShader;
+}
+
+//TODO: fix so they are stored and used correctly
+void ForwardRenderer::SetPixelBuffers(int slot, int numberOfBuffers, ID3D11Buffer* pixelBuffers) {
+	this->pixelBuffers = pixelBuffers;
+}
+
+//TODO: fix so they are stored and used correctly
+void ForwardRenderer::SetVertexBuffers(int slot, int numberOfBuffers, ID3D11Buffer* vertexBuffers) {
+	this->vertexBuffers = vertexBuffers;
 }
 
 //Submits what objects that will be rendered
 void ForwardRenderer::SubmitObject() {
-
+	//store objects in a list (either gameObject, Scene, Model, or something else -steph)
 }
 
-//Cleans up for 2D rendering later
-void ForwardRenderer::EndFrame() {
+
+//Sets everything up for forward rendering
+void ForwardRenderer::BeginFrame(ID3D11DeviceContext* gDeviceContext) {
+	//Clears the color for the scene
+	DirectX::XMFLOAT4 background = {0, 0, 0, 1};
+
+	//Set depth buffer
+
+	//Set buffers that will be used in the pipeline on thier respective shader
+	//TODO: fix so they are stored and used correctly
+	gDeviceContext->VSSetConstantBuffers(0, 1, &this->vertexBuffers);
+	gDeviceContext->PSSetConstantBuffers(0, 1, &this->pixelBuffers);
+
+	//Set shaders
+	//TODO: verify that shaders exist before using them
+	gDeviceContext->IASetInputLayout(inputLayout);
+	gDeviceContext->VSSetShader(vertexShader, nullptr, 0);
+	gDeviceContext->PSSetShader(pixelShader, nullptr, 0);
+
+	//render objects from the object list
+	//this->objects.Render();
+}
+
+//Cleans up gDeviceContext for 2D rendering later
+void ForwardRenderer::EndFrame(ID3D11DeviceContext* gDeviceContext) {
 	//Clean the submit pile for the next frame
 	//Empty and deselect buffers for the 2D render
 }
 
-void ForwardRenderer::CreateVertexShader() {
-	//Creates and saves the vertexShader
-}
-
-void ForwardRenderer::CreatePixelShader() {
-	//Creates and saves the pixelShader
-}
-
-//Creates and read in the bare minimum needed for forward rendering
-ForwardRenderer::ForwardRenderer() noexcept {
-	//Create vertex Shader
-	this->CreateVertexShader();
-	//Create pixel Shader
-	this->CreatePixelShader();
-	//Create G-buffer
-	//Creates depth and depth-Stencil
-}
-
 //Calls private functions to render the frame
-void ForwardRenderer::RenderFrame() {
-	this->BeginFrame();
-	this->EndFrame();
+void ForwardRenderer::RenderFrame(ID3D11DeviceContext* gDeviceContext) {
+	this->BeginFrame(gDeviceContext);
+	this->EndFrame(gDeviceContext);
 }
--- ForwardRenderer.h
+++ ForwardRenderer.h
@@ -1,18 +1,35 @@
 #pragma once
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
 
 class ForwardRenderer {
 private:
-	//ID3D11PixelShader* pixelShader;
-	//ID3D11VertexShader* vertexShader;
-	void BeginFrame();
-	void EndFrame();
-	void CreateVertexShader();
-	void CreatePixelShader();
-	//void CreateGbuffer();
-	//void CreateDepthStencil();
+	//Refrence to shaders that will be used in the pipeline
+	ID3D11InputLayout* inputLayout;
+	ID3D11VertexShader* vertexShader;
+	ID3D11PixelShader* pixelShader;
+
+	//Refrence to buffers which will be used in each shader stage
+	ID3D11Buffer* vertexBuffers;
+	ID3D11Buffer* pixelBuffers;
+
+	//List of objects which need to render
+
+	//Render and clean up functions
+	void BeginFrame(ID3D11DeviceContext* gDeviceContext);
+	void EndFrame(ID3D11DeviceContext* gDeviceContext);
 public:
 	ForwardRenderer() noexcept;
 	virtual ~ForwardRenderer() = default;
+
+	//Will in the future take render objects as argument and store them for render
 	void SubmitObject();
-	void RenderFrame();
+	void RenderFrame(ID3D11DeviceContext* gDeviceContext);
+
+	//Functions for configuring Forward Render
+	void SetVertexShader(ID3D11InputLayout* inputLayout, ID3D11VertexShader* vertexShader);
+	void SetPixelShader(ID3D11PixelShader* pixelShader);
+	void SetVertexBuffers(int slot, int numberOfBuffers, ID3D11Buffer* ppConstantBuffers);
+	void SetPixelBuffers(int slot, int numberOfBuffers, ID3D11Buffer* ppConstantBuffers);
 };
