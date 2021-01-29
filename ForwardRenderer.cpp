4b14f3240e61a46f0ac95d437b136fa93be67cbb
Stephanie <stephaniest1@gmail.com>
2021-01-29 15:30:35 +0100

    fixed light constructor error and Forward Render structure

--- ForwardRenderer.cpp
+++ ForwardRenderer.cpp
@@ -8,12 +8,6 @@ ForwardRenderer::ForwardRenderer() noexcept {
 	this->m_Bloom = Bloom();
 }
 
-//Submits what objects that will be rendered, takes a list of gameObjects as argument
-void ForwardRenderer::SubmitObject() {
-	//Saves them in the vector here
-}
-
-
 //Sets everything up for forward rendering, takes information from the event handler as input
 ID3D11RenderTargetView* ForwardRenderer::BeginFrame(ID3D11RenderTargetView* renderTarget) {
 	/*information that is needed from the event handler and resource manager:
@@ -23,17 +17,21 @@ ID3D11RenderTargetView* ForwardRenderer::BeginFrame(ID3D11RenderTargetView* rend
 	*/
 
 
-	//Clear the render target and set background
+	//Clear the render target and set background here
 
 	//techniques that need to apply before render happens here
 
-	//Set buffers that will be used during this render pass
+	//Set buffers that will be used during this render pass here
 
-	//Set shaders that will be used during this render pass
+	//Set shaders that will be used during this render pass here
 
-	//Render objects from the saved vector
+	//Return frame for post processing
+	return renderTarget;
+}
 
-	//Return frame fo post processing
+//Submits what objects that will be rendered, takes a list of gameObjects as argument
+ID3D11RenderTargetView* ForwardRenderer::SubmitObject(ID3D11RenderTargetView* renderTarget, std::vector<GameObject*> gameObjects) {
+	//go though the vector and render each object to the renderTarget
 	return renderTarget;
 }
 
@@ -43,8 +41,6 @@ ID3D11RenderTargetView* ForwardRenderer::EndFrame(ID3D11RenderTargetView* render
 	* - what renderTarget should we write to? (Will be the one that gets returned as well)
 	*/
 
-	//Empty the submit vector for next frame
-
 	//Apply the post processing effects to the renderTarget here
 
 	//Return the finished frame
@@ -52,7 +48,11 @@ ID3D11RenderTargetView* ForwardRenderer::EndFrame(ID3D11RenderTargetView* render
 }
 
 //Calls private functions to render the frame
-ID3D11RenderTargetView* ForwardRenderer::RenderFrame(ID3D11RenderTargetView* renderTarget) {
-	//return the finished render to the Engine after applying BeginFrame and then Endframe to it.
-	return this->EndFrame(this->BeginFrame(renderTarget));
+ID3D11RenderTargetView* ForwardRenderer::RenderFrame(ID3D11RenderTargetView* renderTarget, std::vector<GameObject*> gameObjects) {
+	//return the finished render to the Engine after applying BeginFrame, SubmitObjects, and then Endframe to it.
+	ID3D11RenderTargetView* target = this->BeginFrame(renderTarget);
+	target = this->SubmitObject(target, gameObjects);
+	return this->EndFrame(target);
+
+	//return this->EndFrame(this->SubmitObject(this->BeginFrame(renderTarget), gameObjects));
 }
