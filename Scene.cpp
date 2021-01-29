17a3afffe1a141c34f525ca674b2804a8a14d8f1
EMIL HÖGSTEDT <emhg18@student.bth.se>
2021-01-29 11:37:30 +0100

    Implemented a Scene class. Used for storing all objects in our game like planets etc.

---
+++ Scene.cpp
@@ -0,0 +1,68 @@
+#include "Scene.h"
+
+Scene::Scene() {
+	this->m_numPlanets = 0;
+}
+
+Scene::~Scene() {
+}
+
+bool Scene::init(unsigned int screenWidth, unsigned int screenHeight, float screenNear, float screenFar, float FOVvalue) {
+	//Orthographic camera. Over the sun.
+	Camera orthoCamera;
+	this->m_orthoCamera = orthoCamera;
+	if (!this->m_orthoCamera.init(screenWidth, screenHeight, screenNear, screenFar, FOVvalue)) {
+
+	}
+
+	//Perspective Camera. Bound to player.
+	PlayerCamera playerCamera;
+	this->m_perspectiveCamera = playerCamera;
+	if (!this->m_perspectiveCamera.init(screenWidth, screenHeight, screenNear, screenFar, FOVvalue)) {
+		//Throw
+		return 0;
+	}
+
+	//Generate sun.
+	/*
+	Sun sun;
+	if(!sun.init()){
+		//Throw
+		return -1;
+	}
+
+	this->m_gameObjects.push_back(sun);
+	*/
+
+	//Get the factory to create the planets.
+	//this->m_factory = ModelFactory::getInstance();
+
+
+	//Behöver ändras pga factory senare.
+	//Generate planets.
+	std::default_random_engine generator;
+	std::uniform_int_distribution<int> distributionPlanets(12, 15);
+	this->m_numPlanets = distributionPlanets(generator);
+	std::uniform_int_distribution<int> distributionRadius(100, 500);
+	/*
+	for(int i = 0; i < this->m_numPlanets; i++){
+		Planet planet;
+		if(!planet.init(i * 1000, 0, 0, distributionRadius(generator))){
+			//Throw
+			return -1;
+		}
+		this->m_gameObjects.push_back(planet);
+	}
+	*/
+
+	return 1;
+}
+
+bool Scene::update() {
+	/*
+	for (auto r : this->m_gameObjects) {
+		r.update();
+	}
+	*/
+	return 1;
+}
