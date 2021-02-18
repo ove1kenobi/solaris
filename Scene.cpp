9659774dda134ce35b0b1f162faf2584d2a6ddd7
EMIL HÖGSTEDT <emhg18@student.bth.se>
2021-02-18 16:08:17 +0100

    .

--- Scene.cpp
+++ Scene.cpp
@@ -68,10 +68,10 @@ bool Scene::init(unsigned int screenWidth, unsigned int screenHeight, Microsoft:
 	using t_clock = std::chrono::high_resolution_clock;
 	std::default_random_engine generator(static_cast<UINT>(t_clock::now().time_since_epoch().count()));
 	std::uniform_int_distribution<int> distributionPlanets(20, 30);
-	this->m_numPlanets = 1/*distributionPlanets(generator)*/;
-	std::uniform_int_distribution<int> distributionRadius(500, 500);
+	this->m_numPlanets = distributionPlanets(generator);
+	std::uniform_int_distribution<int> distributionRadius(100, 500);
 	//World space coordinates
-	std::uniform_int_distribution<int> distributionX(1000, 1000);
+	std::uniform_int_distribution<int> distributionX(1000, 30000);
 	std::uniform_int_distribution<int> distributionY(0, 0);
 	std::uniform_int_distribution<int> distributionZ(0, 0);
 	//Needs to be radians
