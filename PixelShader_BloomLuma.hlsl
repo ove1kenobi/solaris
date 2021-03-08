a3368ccd83ce5c2c42b8b1c62a04133c57cc82e6
EMIL HÖGSTEDT <emhg18@student.bth.se>
2021-03-08 14:25:38 +0100

    Changes to bloom intensity. Added more intervals.

--- PixelShader_BloomLuma.hlsl
+++ PixelShader_BloomLuma.hlsl
@@ -10,9 +10,17 @@ float4 ps_main(in PS_IN psIn) : SV_TARGET
     int3 location = int3(psIn.inPositionSS.x, psIn.inPositionSS.y, 0);
     float4 color = textureToSample.Load(location);
     float brightness = (color.r * 0.2126) + (color.g * 0.7152) + (color.b * 0.0722);
-    if (brightness > 0.6 && brightness < 0.9)
+    if (brightness > 0.6 && brightness <= 0.7)
     {
-        return color;
+        return color * 0.125;
+    }
+    else if (brightness > 0.7 && brightness <= 0.8)
+    {
+        return color * 0.25f;
+    }
+    else if (brightness > 0.8 && brightness <= 1.0)
+    {
+        return color * 0.5f;
     }
     else
     {
